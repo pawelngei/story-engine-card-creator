e869d83c9e2ba4dda0389702ec36d77de2f287ea
alxd <github@alxd.org>
2020-05-15 19:10:10 +0200

    fix CSV to allow newLines

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -1,11 +1,17 @@
 import { CardType } from '../Card/Card'
 
+const nlRegex = /\n/gi
+const pipeRegex = /\|/gi
+
+const newLineToPipe = (str: string) => str.replace(nlRegex, '|')
+const pipeToNewLine = (str: string) => str.replace(pipeRegex, '\n')
+
 export const exportCSV = (cards: CardType[]) => {
   const cardRows = cards.map(card => ([
     card.type, card.bottom, card.top, card.left, card.right
   ]))
   const csvContent = 'Type,Bottom,Top,Left,Right\n'
-    + cardRows.map(e => e.join(",")).join("\n")
+    + cardRows.map(e => newLineToPipe(e.join(","))).join("\n")
   return csvContent
 }
 
@@ -15,10 +21,10 @@ export const importCSV = (csvContent: string) => {
     const columns = row.split(',')
     const card = {
       type: columns[0],
-      bottom: columns[1],
-      top: columns[2],
-      left: columns[3],
-      right: columns[4]
+      bottom: pipeToNewLine(columns[1] || ''),
+      top: pipeToNewLine(columns[2] || ''),
+      left: pipeToNewLine(columns[3] || ''),
+      right: pipeToNewLine(columns[4] || '')
     }
     return card
   })
