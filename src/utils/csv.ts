bece3c85f9eafb470ef89f1cf76290a7af211c37
alxd <github@alxd.org>
2020-05-15 19:31:30 +0200

    use semicolons instead of commas to separate values

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -11,14 +11,14 @@ export const exportCSV = (cards: CardType[]) => {
     card.type, card.bottom, card.top, card.left, card.right
   ]))
   const csvContent = 'Type,Bottom,Top,Left,Right\n'
-    + cardRows.map(e => newLineToPipe(e.join(","))).join("\n")
+    + cardRows.map(e => newLineToPipe(e.join(";"))).join("\n")
   return csvContent
 }
 
 export const importCSV = (csvContent: string) => {
   const csvRows = csvContent.split('\n')
   const cards = csvRows.slice(1).map(row => {
-    const columns = row.split(',')
+    const columns = row.split(';')
     const card = {
       type: columns[0],
       bottom: pipeToNewLine(columns[1]),
